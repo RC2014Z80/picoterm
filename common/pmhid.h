ebd36cd413551a16d6be320a962c8f8d42061d33
dmeurisse <info@mchobby.be>
2022-10-22 11:02:58 +0200

    remove tinyusb header (it is not a tinyusb source file)

--- common/pmhid.h
+++ common/pmhid.h
@@ -1,39 +1,27 @@
 /*
- * The MIT License (MIT)
+ *  KEYCODE to Ascii Conversion
  *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
+ *  This file contains the various Keyboard Layout declaration supported by
+ *  PicoTerm.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Expand to array of [128][3]
+ *                       |   +-> (ascii without shift, ascii with shift, ascii with right alt)
+ *                       +-----> HID_KEYCODE (0..127)
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Usage: example to convert ascii from keycode (key) and shift modifier (shift).
+ * Here we assume key < 128 ( printable )
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ *  uint8_t const conv_table[128][2] =  { HID_KEYCODE_TO_ASCII };
+ *  char ch = shift ? conv_table[chr][1] : conv_table[chr][0];
  *
- * This file is part of the TinyUSB stack.
- */
-
-/** \ingroup group_class
- *  \defgroup ClassDriver_HID Human Interface Device (HID)
- *  @{ */
+*/
 
 #ifndef _PM_HID_H_
 #define _PM_HID_H_
 
 #include "common/tusb_common.h"
 
-// Choose the mapping @ make call (eg: make picoterm, make picoterm_US, make all)
+// Choose the mapping @ "make" call (eg: make picoterm, make picoterm_US, make all)
 // See CMakeLists.txt for more informations
 // Copy and paste an existing mapping to make a new one, see the
 
@@ -42,18 +30,6 @@
  extern "C" {
 #endif
 
-
-/*--------------------------------------------------------------------
- * KEYCODE to Ascii Conversion
- *  Expand to array of [128][3] (ascii without shift, ascii with shift, ascii with right alt)
- *
- * Usage: example to convert ascii from keycode (key) and shift modifier (shift).
- * Here we assume key < 128 ( printable )
- *
- *  uint8_t const conv_table[128][2] =  { HID_KEYCODE_TO_ASCII };
- *  char ch = shift ? conv_table[chr][1] : conv_table[chr][0];
- *
- *--------------------------------------------------------------------*/
 
 //--------------------------------------------------------------------/
 // US keyboard
